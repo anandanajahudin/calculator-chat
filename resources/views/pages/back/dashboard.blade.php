ab1c78e0c71a7535d07c261d7d6399c1ab144cd0
anandanajahudin <ahmadananda96@gmail.com>
2023-11-27 15:40:07 +0700

    update dashboard

--- resources/views/pages/back/dashboard.blade.php
+++ resources/views/pages/back/dashboard.blade.php
@@ -20,13 +20,14 @@
     <div class="card">
         <div class="card-body">
             <h5 class="card-title fw-semibold mb-4">Calculator Chat</h5>
-            @include('layouts.components.back.tools')
+
             <form action="{{ route('calculator.store') }}" method="POST">
                 @csrf
                 <div class="input-group">
-                    <input type="text" class="form-control" name="chat" id="chat" required placeholder="Enter Queries" aria-label="Queries">
-                    <a href="javascript:showhide('uniquename')" id="button" type="button"  class="btn btn-warning" >
-                    <i class="fa-solid fa-keyboard"></i></a>
+                    <input type="text" class="form-control" name="chat" id="chat" required
+                        placeholder="Enter Queries" aria-label="Queries">
+                    {{-- <a href="javascript:showhide('uniquename')" id="button" type="button" class="btn btn-warning">
+                        <i class="fa-solid fa-keyboard"></i></a> --}}
                     <button class="btn btn-primary" type="submit">Submit</button>
                 </div>
 
